86ce0bcba2dd34d2116d3c97a1afdeaed3fa3d1d
Sameer Ahmed <sameer03054@gmail.com>
2022-10-02 12:16:51 +0500

    minor changes in React template

--- 01-create-react-app/src/App.js
+++ 01-create-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sameer Ahmed <code>Creating</code> his first React App.
         </p>
         <a
           className="App-link"
